e3e5416b23462ac84c09c6c785112cae20dda9ad
Harnoor singh <singhharnoor116@gmail.com>
2024-03-22 07:05:10 +0530

    AppBar added

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,15 +1,20 @@
+import { AppBar } from "../components/AppBar";
 import { BlogCard } from "../components/BlogCard";
 
 export const Blogs = () => {
   return (
     <div>
-      <BlogCard
-        authorName={"adsf"}
-        title={"title of the blog"}
-        content={"Content of the blog"}
-        publishedDate="2nd Feb 2024"
-      />
-      ;
+      <AppBar />
+      <div className="flex justify-center">
+        <div className="max-w-xl">
+          <BlogCard
+            authorName={"adsf"}
+            title={"title of the blog"}
+            content={"Content of the blog"}
+            publishedDate="2nd Feb 2024"
+          />
+        </div>
+      </div>
     </div>
   );
 };
